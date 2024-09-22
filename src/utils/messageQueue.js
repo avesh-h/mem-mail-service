df28bce958e5b5d86caded442b42cca767325eeb
Avesh <aveshhasanfatta1155@gmail.com>
2024-09-22 20:14:07 +0530

    fix : reconnection issue when node process down

--- src/utils/messageQueue.js
+++ src/utils/messageQueue.js
@@ -9,9 +9,22 @@ const createChannel = async () => {
     const conn = await amqplib.connect(MESSAGE_BROKER_URL);
     const channel = await conn.createChannel();
     await channel.assertExchange(EXCHANGE_NAME);
+
+    // Handle connection errors
+    conn.on("error", (err) => {
+      console.error("Connection error:", err);
+    });
+
+    // Handle unexpected connection closures and reconnect
+    conn.on("close", () => {
+      console.warn("Connection to RabbitMQ closed, reconnecting...");
+      setTimeout(createChannel, 1000); // Attempt to reconnect after 1 second
+    });
+
     return channel;
   } catch (error) {
-    return error;
+    console.error("Error in creating channel:", error);
+    setTimeout(createChannel, 1000); // Reattempt after 1 second on failure
   }
 };
 
